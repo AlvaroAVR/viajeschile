838e31184728cb845b61f8ca4b9dae2d6b385d45
avargas <alvaro20vr@gmail.com>
2022-04-13 12:42:19 -0400

    detalles en javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,24 @@
+// activacion tooltop
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-
+// funcion que valida que los datos ingresados no superen cierta longitud
 function validar(){
     var nombre = document.getElementById("nombreInput").value;
-    alert(nombre);
     var asunto = document.getElementById('inputAsunto').value;
-    alert(asunto)
     var mensaje = document.getElementById('areaTexto').value;
-    alert(mensaje)
-    var mensaje = '';
-    var cont = 0;
 
-    // }else{
-    //     if(nombre.length > 15){
-    //         mensaje = ' El nombre no puede superar los 15 caracteres';
-    //         alert(mensaje);
-    //         cont = cont + 1;
-    //     }
-    //     if(asunto.length > 30){
-    //         mensaje = 'El asunto no puede superar los 30 caracteres';
-    //         alert(mensaje);
-    //         cont = cont + 1;
-    //     }
-    //     if(mensaje.length > 150){
-    //         mensaje = 'El mensaje no puede superar los 150 caracteres';
-    //         alert(mensaje);
-    //         cont = cont + 1;
-    //     }
-    // }
-    // if(cont == 3){
-    //     alert(nombre + "Su mensaje a sido enviado con exito");
-    //     return true;
-    // }
-    // return false;
+    if(nombre.length > 15){
+      alert('El nombre no puede superar los 15 caracteres');
+    }
+    if(asunto.length > 30){
+      alert('El asunto no puede superar los 30 caracteres');
+    }
+    if(mensaje.length > 300){
+      alert('El mensaje no puede superar los 300 caracteres');
+    }
 }
 
 
